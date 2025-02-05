2732e93fa9f11adde4d8b5e58c60d92b7ea15d5c
ponmarimdeiveega@gmail.com <ponmarimdeiveega@example.com>
2025-02-05 15:02:26 +0530

    axios correction added

--- src/pages/UrlTable.jsx
+++ src/pages/UrlTable.jsx
@@ -1,44 +1,48 @@
 import axios from 'axios';
-export default function UrlTable(){
-  axios.get("http://localhost:4000/urlshort/allURL")
-  .then(function(response){
-    console.log(response)
-  })
-  .catch(function(error){
-    console.log(error)
-  })
-  .finally(function(){
-    console.log("something went wrong")
-  })
-    return(
-        <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Category</th>
-      <th scope="col">Big URL</th>
-      <th scope="col">Short URL</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-    )
+import { useEffect } from 'react';
+export default function UrlTable() {
+  useEffect(() => {
+    axios.get("http://localhost:4000/urlshort/allURL")
+      .then(function (response) {
+        console.log(response)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+      .finally(function () {
+        console.log("something went wrong")
+      })
+  }, []);
+
+  return (
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Category</th>
+          <th scope="col">Big URL</th>
+          <th scope="col">Short URL</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
+  )
 }
